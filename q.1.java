393006599a2b4413fa648f919dee1a0db5e194e6
Shalini199 <84658661+Shalini199@users.noreply.github.com>
2021-05-23 15:24:06 +0530

    Add files via upload

---
+++ q.1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class sample
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(system.in);
+System.out.println("Enter the number of element ");
+int n=sc.nextInt();
+System.out.println("Enter words ");
+String[]w=sc.nextLine();
+int size=w.length;
+if(n<=0)
+{
+System.out.println("Invalid input ");
+}
+else 
+for(int i=0;i<w-1;i++)
+{
+if
