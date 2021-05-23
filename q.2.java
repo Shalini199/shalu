816e384f7ced7b519085ccd9345f803ca7d2a866
Shalini199 <84658661+Shalini199@users.noreply.github.com>
2021-05-23 15:56:46 +0530

    Add files via upload

---
+++ q.2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class sample
+{
+public static void main(String args[])
+{
+int upper=0,lower=0,special=0,digit=0;
+Scanner sc=new Scanner(system.in);
+System.out.println("Enter the lenght of password ");
+int n=sc.nextInt();
+System.out.println("Enter the password ");
+for(int i=0;i<pass.length();i++)
+{
+char ch =str.charAt(i);
+if (ch>='A' && ch<='z')
+upper++;
+else if (ch>='a' && ch<='z')
+lower++;
+else if (ch>='0' && ch<='9')
+number++;
+String pass=sc.nextLine();
+System.out.println("password contains");
+
+System.out.println("Special character="+count);
+
